bc30a98235bdaa4a459f904ff7b1919f4abb60bc
Guillaume Chevalier <guillaume-chevalier@hotmail.com>
2015-12-24 19:02:45 -0500

    Added cifar-10 dataset conversion to HDF5

--- README.md
+++ README.md
@@ -3,15 +3,30 @@ Both deep learning datasets can be imported in python directly with h5py (HDF5 f
 
 ***NOTE: The training set has been shuffled and put in a big single batch, it was originally splitted into 5 files and not shuffled. The test set has not been shuffled and remained intact.***
 
-## A. Direct import in caffe
+
+## How to use
+
+### Method A: Direct import in caffe
 You can simply download the zip file for the desired dataset and ignore the rest of this repository.
 
-## B. Download and convert manually with the Python script
+### Method B: Download and convert manually with the Python script
 You can run my python script for the desired dataset. This way, you will not need to download the whole repository, but only the desired python script. It will automatically download and convert the dataset for caffe in the respective subfolder.
 My script, however, has many python dependencies, if you can `import caffe` in python, you should normally have all the required libs, although caffe is never imported in the scripts I provide.
-
-### Not shuffling datasets
+#### Not shuffling datasets
 If you do not want the datasets to be shuffled, my scripts can be a good starting point for the conversion of the cifar 10 and 100 datasets to the HDF5 caffe format. You may refer to this interesting tutorial to understand better how to do the conversion: https://github.com/BVLC/caffe/blob/master/examples/02-brewing-logreg.ipynb
+
+
+## Datasets shape info
+
+### cifar-10
+Each element are of shape `3*32*32`:
+`print(X.shape)` --> `(50000, 3, 32, 32)`
+
+From the Caffe documentation:
+"*The conventional blob dimensions for batches of image data are **number N x channel K x height H x width W**.*"
+
+### cifar-100
+[TODO]
 
 ## Link to original datasets
 https://www.cs.toronto.edu/~kriz/cifar.html
---
+++ download-and-convert-cifar-10.py
@@ -0,0 +1,111 @@
+import copy
+import os
+from subprocess import call
+
+import numpy as np
+import sklearn
+import sklearn.linear_model
+
+import h5py
+
+print("")
+
+print("Downloading...")
+if not os.path.exists("cifar-10-python.tar.gz"):
+    call(
+        "wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz",
+        shell=True
+    )
+    print("Downloading done.\n")
+else:
+    print("Dataset already downloaded. Did not download twice.\n")
+
+
+print("Extracting...")
+cifar_python_directory = os.path.abspath("cifar-10-batches-py")
+if not os.path.exists(cifar_python_directory):
+    call(
+        "tar -zxvf cifar-10-python.tar.gz",
+        shell=True
+    )
+    print("Extracting successfully done to {}.".format(cifar_python_directory))
+else:
+    print("Dataset already extracted. Did not extract twice.\n")
+
+
+print("Converting...")
+cifar_caffe_directory = os.path.abspath('cifar_10_caffe_hdf5/')
+if not os.path.exists(cifar_caffe_directory):
+
+    def unpickle(file):
+        import cPickle
+        fo = open(file, 'rb')
+        dict = cPickle.load(fo)
+        fo.close()
+        return dict
+
+    def load_train_and_shuffle(train_batches):
+        data = []
+        labels = []
+        for data_batch_i in train_batches:
+            d = unpickle(
+                os.path.join(cifar_python_directory, data_batch_i)
+            )
+            data.append(d['data'])
+            labels.append(np.array(d['labels']))
+        # Merge training batches on their first dimension
+        data = np.concatenate(data)
+        labels = np.concatenate(labels)
+
+        # Shuffle
+        data, _, labels, _ = sklearn.cross_validation.train_test_split(
+            data, labels, test_size=0.0, random_state=42
+        )
+        return data.reshape(50000, 3, 32, 32), labels
+
+    def load_test(test_batch):
+        d = unpickle(os.path.join(cifar_python_directory, test_batch))
+        data = d['data']
+        labels = np.array(d['labels'])
+        return data.reshape(10000, 3, 32, 32), labels
+
+
+    X, y = load_train_and_shuffle(
+        ["data_batch_{}".format(i) for i in range(1, 6)]
+    )
+
+    Xt, yt = load_test("test_batch")
+
+    print("INFO: each dataset's element are of shape 3*32*32:")
+    print('"print(X.shape)" --> "{}"\n'.format(X.shape))
+    print("From the Caffe documentation: ")
+    print("The conventional blob dimensions for batches of image data "
+          "are number N x channel K x height H x width W.\n")
+
+    os.makedirs(cifar_caffe_directory)
+    train_filename = os.path.join(cifar_caffe_directory, 'train.h5')
+    test_filename = os.path.join(cifar_caffe_directory, 'test.h5')
+
+    # "train.txt" is an "HDF5DataLayer source"
+    # containing a list of the HDF5 filenames. See:
+    # https://github.com/BVLC/caffe/blob/master/examples/02-brewing-logreg.ipynb
+
+    comp_kwargs = {'compression': 'gzip', 'compression_opts': 1}
+
+    # Train
+    with h5py.File(train_filename, 'w') as f:
+        f.create_dataset('data', data=Xt, **comp_kwargs)
+        f.create_dataset('label', data=yt.astype(np.float32), **comp_kwargs)
+    with open(os.path.join(cifar_caffe_directory, 'train.txt'), 'w') as f:
+        f.write(train_filename + '\n')
+
+    # Test
+    with h5py.File(test_filename, 'w') as f:
+        f.create_dataset('data', data=Xt, **comp_kwargs)
+        f.create_dataset('label', data=yt.astype(np.float32), **comp_kwargs)
+    with open(os.path.join(cifar_caffe_directory, 'test.txt'), 'w') as f:
+        f.write(test_filename + '\n')
+
+    print("Conversion successfully done to {}.\n".format(cifar_caffe_directory))
+else:
+    print("Conversion was already done. Did not convert twice.\n")
